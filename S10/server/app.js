ddcd0556d3903d80737652e086db14e78055b9c8
Kabir <sagithedeveloper@gmail.com>
2025-09-02 17:58:13 +0530

    Added the code

---
+++ S10/server/app.js
@@ -0,0 +1,49 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "POST" && req.url === "/create-new-user") {
+    req.on("data", (request_data) => {
+      //Write the logic to perfrom action that you want to do with request data
+      const newuser = JSON.parse(request_data);
+      fs.readFile("users.json", "utf-8", (error, data) => {
+        if (error) {
+          res.end("Something went wrong");
+          console.log(error.message);
+        } else {
+          const objectData = JSON.parse(data);
+          //   console.log(typeof objectData);
+          objectData.results.push(newuser);
+          const userObject = JSON.stringify(objectData);
+
+          fs.writeFile("users.json", userObject, (error) => {
+            if (error) {
+              res.end("Failed to Create User");
+              console.log(error.message);
+            } else {
+              res.end("User Created Successfully");
+            }
+          });
+        }
+      });
+    });
+  } else if (req.method === "GET" && req.url === "/get-users") {
+    fs.readFile("users.json", "utf-8", (error, data) => {
+      if (error) {
+        res.end("{ok:false,error:'Failed to Access Users Data'}");
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    res.end("Invalid Request,Only Post Request is Allowed");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server Started on 3000 Port Number");
+});
+
+//http://localhost:3000
